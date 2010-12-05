ee73d85565470ec4547b04014843f6c26468cf3c
Jesse Curry <jesse@jessecurry.net>
2010-12-05 16:10:10 -0500

    Setup basic project structure

---
+++ src/KLConstants.h
@@ -0,0 +1,9 @@
+/*
+ *  KLConstants.h
+ *  keylime
+ *
+ *  Created by Jesse Curry on 12/5/10.
+ *  Copyright 2010 Jesse Curry. All rights reserved.
+ *
+ */
+
---
+++ src/keylime.h
@@ -0,0 +1,17 @@
+/*
+ *  keylime.h
+ *  keylime
+ *
+ *  Created by Jesse Curry on 12/5/10.
+ *  Copyright 2010 Jesse Curry. All rights reserved.
+ *
+ */
+
+#pragma mark -
+#import "KLConstants.h"
+
+#pragma mark -
+#import "KLCategories"
+
+#pragma mark -
+#import "KLViewControllers.h"
